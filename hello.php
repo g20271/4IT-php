cfa37cb9e8645f170901530f1d27f4da186d3c14
g20271 <g20271@kagawa.kosen-ac.jo>
2023-08-08 02:17:55 +0000

    fix

---
+++ hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang = "ja">
+    <head>
+        <meta charset = "utf-8">
+        <title> PHP link list </title>
+    </head>
+    <body>
+        <h1> PHP Link List </h1>
+        <hr>
+        <a href="hello.php">PHPプログラム</a><br>
+        <br>
+        <address>自分のメールアドレス</address>
+    </body>
+</html>
